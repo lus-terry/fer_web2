88fa61c3b087ee5cc02ed3905a9c14055ae39a73
lus-terry <lucija.klanjac@gmail.com>
2024-10-16 17:32:43 +0200

    ready for render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,16 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { getTickets } from './db';
+import fs from 'fs';
+import https from 'https';
 
 dotenv.config();
 
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 // Middleware za parsiranje JSON tijela
 app.use(express.json());
@@ -29,7 +34,27 @@ app.get('/api', async (req: Request, res: Response) => {
   }
 });
 
-// Pokretanje servera
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+
+// Konfiguracija servera
+if (externalUrl) {
+  // Ako smo u Render okruženju (externalUrl je dostupan)
+  const hostname = '0.0.0.0'; // Potrebno za pokretanje na Renderu
+  app.listen(port, hostname, () => {
+    console.log(`Server locally running at http://${hostname}:${port}/ and from
+      outside on ${externalUrl}`);
+  });
+} else {
+  // Ako smo lokalno, pokreni HTTPS server
+  /*https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app)
+  .listen(port, function() {
+    console.log(`Server running at https://localhost:${port}`);
+  });*/
+  
+    // Ako smo lokalno, koristi samo HTTP (ne HTTPS)
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+}
